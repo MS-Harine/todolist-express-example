d6a302d8a1887637f6dd2e61b1e269d79e012f8d
MS-Harine <Minseok.H.Song@gmail.com>
2021-07-07 03:23:14 +0900

    Finish login and functions

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const session = require('express-session');
+const mysqlStore = require('express-mysql-session')(session);
+const { sequelize } = require('./models');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/config/config.json')[env];
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+sequelize
+	.authenticate()
+	.then(() => console.log('Connection Created!'))
+	.catch((err) => { console.log(err); });
+sequelize.sync({force: false});
+const sessionStore = new mysqlStore({
+	host: config.host,
+	port: 3306,
+	user: config.username,
+	password: config.password,
+	database: config.database
+});
 
-var app = express();
+const app = express();
+const router = require('./routes');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({
+	secret: 'my custom secret',
+	resave: false,
+	saveUninitialized: true,
+	store: sessionStore,
+	cookie: {
+		secure: false,
+		httpOnly: false,
+	},
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
+app.use('/', router);
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const userRouter = require('./user');
+const todoRouter = require('./todolist');
+
+router.use('/user', userRouter);
+router.use('/todolist', todoRouter);
 
 module.exports = router;
